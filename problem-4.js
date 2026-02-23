6cd4282f701e2b46ec209dd0502d9707bc958814
forthright47 <k.tuhinjaman@gmail.com>
2026-02-24 02:03:17 +0600

    added the solution of Problem-04: Upcoming Gono Vote

---
+++ problem-4.js
@@ -0,0 +1,25 @@
+//Problem-04: Upcoming Gono Vote
+function gonoVote(array) {
+    if (!Array.isArray(array)) {
+        return "Invalid";
+    }
+
+    let haCount = 0;
+    let naCount = 0;
+
+    for (let vote of array) {
+        if (vote === "ha") {
+            haCount++;
+        } else if (vote === "na") {
+            naCount++;
+        }
+    }
+
+    if (haCount > naCount) {
+        return true;
+    } else if (haCount === naCount) {
+        return "equal";
+    } else {
+        return false;
+    }
+}
