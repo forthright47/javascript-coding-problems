baebe5ca3204d40c13459bae5904fef47a7acded
forthright47 <k.tuhinjaman@gmail.com>
2026-02-24 02:01:44 +0600

    added the solution of Problem-02: OTP Validation for Zapshift

---
+++ problem-2.js
@@ -0,0 +1,16 @@
+//Problem-02: OTP Validation for Zapshift
+function validOtp(otp) {
+    if (typeof otp !== "string") {
+        return "Invalid";
+    }
+
+    if (otp.length !== 8) {
+        return false;
+    }
+
+    if (!otp.startsWith("ph-")) {
+        return false;
+    }
+
+    return true;
+}
